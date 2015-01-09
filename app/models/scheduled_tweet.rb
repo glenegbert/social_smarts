50f785d2e6cb722d069fbeb87be8612c9cf4a64a
Glen Egbert <glenegbert1@gmail.com>
2015-01-09 15:31:34 -0700

    refactored ScheduledTweet model

--- app/models/scheduled_tweet.rb
+++ app/models/scheduled_tweet.rb
@@ -1,10 +1,15 @@
 class ScheduledTweet < ActiveRecord::Base
 
   def scheduled_date_and_time_combined
-    time = self.time[10..-1]
-    date = self.date[0..9]
-    date_time = date + time
-    Time.parse(date_time)
+    Time.parse(scheduled_date[0..9] + scheduled_time[10..-1])
+  end
+
+  def scheduled_time
+    self.time
+  end
+
+  def scheduled_date
+    self.date
   end
 
 end
--- app/models/scheduled_tweet_sender.rb
+++ app/models/scheduled_tweet_sender.rb
@@ -2,7 +2,7 @@ class ScheduledTweetSender
 
   def self.send_tweets
     ScheduledTweet.all.each do |tweet|
-      if tweet.to_datetime < Time.zone.now
+      if tweet.scheduled_date_and_time_combined < Time.zone.now
         user = User.find(tweet.user_id)
         user.client.update(tweet.text)
         tweet.delete
