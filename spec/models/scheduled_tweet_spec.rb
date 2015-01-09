7d455457037dc170c42252f8f7cfd83ed365550d
Glen Egbert <glenegbert1@gmail.com>
2015-01-09 10:54:38 -0700

    WIP scheduled tweet model test

--- spec/models/scheduled_tweet_spec.rb
+++ spec/models/scheduled_tweet_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe ScheduledTweet, :type => :model do
 
   describe "combines scheduled date and time to create a date_time" do
-    scheduled_tweet = ScheduledTweet.create(text: "Yo", user_id: "1", time: "1970-01-01T21:57:00.000Z", date:)
+    scheduled_tweet = ScheduledTweet.create(text: "Yo", user_id: "1", time: "1970-01-01T21:57:00.000Z",
+     date: "2015-01-09T17:51:53.837Z")
   end
 
 end
