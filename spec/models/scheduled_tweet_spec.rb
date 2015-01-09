896845b485ede6f0d7e1f7b96db84f4c1777e704
Glen Egbert <glenegbert1@gmail.com>
2015-01-09 10:53:16 -0700

    fixed scheduled tweet bug

--- spec/models/scheduled_tweet_spec.rb
+++ spec/models/scheduled_tweet_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ScheduledTweet, :type => :model do
+
+  describe "combines scheduled date and time to create a date_time" do
+    scheduled_tweet = ScheduledTweet.create(text: "Yo", user_id: "1", time: "1970-01-01T21:57:00.000Z", date:)
+  end
+
 end
