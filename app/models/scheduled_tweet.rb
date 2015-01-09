f944c18ca99c6a9e82fb92026a4e8be598874454
Glen Egbert <glenegbert1@gmail.com>
2015-01-09 15:26:53 -0700

    changed name of to_datetime_method and added additional model test

--- app/models/scheduled_tweet.rb
+++ app/models/scheduled_tweet.rb
@@ -1,6 +1,6 @@
 class ScheduledTweet < ActiveRecord::Base
 
-  def to_datetime
+  def scheduled_date_and_time_combined
     time = self.time[10..-1]
     date = self.date[0..9]
     date_time = date + time
--- spec/models/scheduled_tweet_spec.rb
+++ spec/models/scheduled_tweet_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe ScheduledTweet, :type => :model do
 
-  describe "combines scheduled date and time to create a date_time" do
+  it "creates scheduled tweets" do
     scheduled_tweet = ScheduledTweet.create(text: "Yo", user_id: "1", time: "1970-01-01T21:57:00.000Z",
-     date: "2015-01-09T17:51:53.837Z")
+                                            date: "2015-01-09T17:51:53.837Z")
+    expect(scheduled_tweet).to be_valid
+
+  end
+
+
+  it "combines scheduled date and scheduled time objects" do
+   scheduled_tweet = ScheduledTweet.create(text: "Yo", user_id: "1", time: "1970-01-01T21:57:00.000Z",
+                                           date: "2015-01-09T17:51:53.837Z")
+
+     expect(scheduled_tweet.scheduled_date_and_time_combined).to eq("2015-01-09 21:57:00 UTC")
   end
 
 end
